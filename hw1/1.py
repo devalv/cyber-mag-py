b04334701ad7fb31703aade603927c55315712e8
Aleksey Devyatkin <aleksei@devyatkin.dev>
2024-11-12 23:00:12 +0300

    hw1: 1.1 updates

--- hw1/1.py
+++ hw1/1.py
@@ -1,15 +1,22 @@
 def middle_printer(inp: str) -> str:
-    # validation
+    """
+    Return the middle character of the input string, or the two middle characters if the string length is even.
+
+    :param inp: The string to find the middle character in.
+    :return: The middle character of the string, or the two middle characters if the string length is even.
+    """
     assert inp
     assert isinstance(inp, str)
 
-    len_is_even: int = len(inp) % 2
-    return inp[len(inp) // 2] if len_is_even else inp[len(inp) // 2 - 1 : len(inp) // 2 + 1]
+    return inp[len(inp) // 2] if len(inp) % 2 else inp[len(inp) // 2 - 1 : len(inp) // 2 + 1]
 
 
 def main():
     assert middle_printer('test'), 'es'
     assert middle_printer('testing'), 't'
+    assert middle_printer('tes'), 'e'
+    assert middle_printer('te'), 'te'
+    assert middle_printer('t'), 't'
     try:
         middle_printer('')
     except AssertionError:
